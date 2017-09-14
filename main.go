727e5897d683f370b83d125fa23bc224a20a6e5c
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2017-09-14 19:42:06 +0530

    Adding spf13/viper support + sample config yml

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
 
 func main() {
+	viper.AddConfigPath(".") // always look for config.{json,yml,toml,hcl} in the current working directory
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
 	r := gin.Default()
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -11,4 +22,3 @@ func main() {
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
-
