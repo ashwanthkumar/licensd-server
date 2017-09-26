3ed988250555a1b794d6c8bc5fb526387ef87475
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2017-09-27 00:19:15 +0530

    typos and minor refactoring

--- parser/license_finder_test.go
+++ parser/license_finder_test.go
@@ -4,7 +4,7 @@ import "testing"
 import "github.com/stretchr/testify/assert"
 
 func TestParseLineForAnEntry(t *testing.T) {
-	sbt := LicenseFinder{}
+	lf := LicenseFinder{}
 	line := `cascading:cascading-core, 2.5.5, "Apache 2.0"`
 	expectedLicense := &License{Name: "Apache 2.0"}
 	expectedDep := &Dependency{
@@ -12,7 +12,7 @@ func TestParseLineForAnEntry(t *testing.T) {
 		Version:  "2.5.5",
 		Licenses: []*License{expectedLicense},
 	}
-	dep, err := sbt.parseLine(line)
+	dep, err := lf.parseLine(line)
 
 	assert.NoError(t, err)
 	assert.EqualValues(t, expectedDep.Licenses, dep.Licenses)
